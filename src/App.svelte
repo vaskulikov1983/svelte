edc324567181f9c2f793b35b4f58fee811ac34c7
Vasiliy Kulikov <vas.kulikov1983@gmail.com>
2021-04-12 21:09:56 +0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	let name = 'Vasiliy';
+	let age = 30;
+
+	const counter = () => age++;  
+	$: uppercaseName = name.toUpperCase();
+	$: console.log(name);
+	$: if (name === 'Fedor') {
+		age = 34;
+	}
+
+	const changeName = () => name = 'Fedor';
+
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}, and my age is {age}!</h1>
+<button on:pointerdown = {counter}>Change age</button>
+<button on:pointerdown="{changeName}">Change name</button>
+
